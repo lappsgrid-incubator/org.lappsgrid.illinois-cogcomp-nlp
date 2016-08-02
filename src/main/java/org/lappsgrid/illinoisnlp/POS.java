181833bb81c91c729bb0ff5ec36dc22b23349efc
Baian Liu <baliu@vassar.edu>
2016-08-01 20:10:42 -0400

    Added ability to pass through input as Data objects and output as well

--- src/main/java/org/lappsgrid/illinoisnlp/POS.java
+++ src/main/java/org/lappsgrid/illinoisnlp/POS.java
@@ -8,9 +8,18 @@ import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TokenLabelView;
 import edu.illinois.cs.cogcomp.pos.POSAnnotator;
 import org.lappsgrid.api.ProcessingService;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.DataContainer;
+import org.lappsgrid.serialization.Serializer;
+import org.lappsgrid.serialization.lif.Annotation;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.View;
+import org.lappsgrid.vocabulary.Features;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class POS implements ProcessingService {
 
@@ -26,11 +35,47 @@ public class POS implements ProcessingService {
 
     @Override
     public String execute(String input) {
+        // Step #1: Parse the input.
+        Data data = Serializer.parse(input, Data.class);
+
+        // Step #2: Check the discriminator
+        final String discriminator = data.getDiscriminator();
+        if (discriminator.equals(Discriminators.Uri.ERROR)) {
+            // Return the input unchanged.
+            return input;
+        }
+
+        // Step #3: Extract the data.
+        Container container;
+        if (discriminator.equals(Discriminators.Uri.TOKEN) ||
+                discriminator.equals(Discriminators.Uri.LAPPS)) {
+            container = new Container((Map) data.getPayload());
+        } else {
+            // This is a format we don't accept.
+            String message = String.format("Unsupported discriminator type: %s", discriminator);
+            return new Data<>(Discriminators.Uri.ERROR, message).asJson();
+        }
+
+        // Get the tokens from the input and turn them into Mallet's desired format
+        View view = new View(container.getView(0));
+        List<Annotation> annotations = view.getAnnotations();
+        int numTokens = annotations.size();
+
+        String[] tokens = new String[numTokens];
+        for (int i = 0; i < numTokens; i++) {
+            String token = annotations.get(i).getFeature(Features.Token.WORD);
+            if (token != null) {
+                tokens[i] = token;
+            } else {
+                return new Data<>(Discriminators.Uri.ERROR, "Found non-tokens.").asJson();
+            }
+        }
 
         POSAnnotator posAnnotator = new POSAnnotator();
         List<String[]> tokenizedSentences = new ArrayList<>();
-        String[] sent1 = {"I", "am", "testing", "this", "on", "a", "Monday", "."};
-        tokenizedSentences.add(sent1);
+
+        tokenizedSentences.add(tokens);
+
         TextAnnotation ta =
                 BasicTextAnnotationBuilder.createTextAnnotationFromTokens(tokenizedSentences);
         try {
@@ -40,11 +85,22 @@ public class POS implements ProcessingService {
             return "Unable to annotate.";
         }
 
-        TokenLabelView view = (TokenLabelView) ta.getView(ViewNames.POS);
+        TokenLabelView labelView = (TokenLabelView) ta.getView(ViewNames.POS);
+        View resultsView = new View();
+        for (int i = 0; i < labelView.getTextAnnotation().size(); i++){
+            Annotation a = annotations.get(i);
+            a.addFeature(Features.Token.POS, labelView.getLabel(i));
+            resultsView.add(a);
+        }
 
-        System.out.println(view);
+        resultsView.addContains(Discriminators.Uri.POS, this.getClass().getName(), "part of speech");
 
-        return null;
+        Container resultsContainer= new Container();
+        resultsContainer.setText(container.getText());
+        resultsContainer.addView(resultsView);
+        data = new DataContainer(resultsContainer);
+
+        return data.asPrettyJson();
     }
 }
 
